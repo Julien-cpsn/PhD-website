b1dc8ce0233ad60c4c51df3b8148e1522ddf81e6
Julien-cpsn <julien.caposiena@gmail.com>
2025-02-21 13:56:52 +0100

    Improved publications page

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -9,12 +9,18 @@ function getPublications() {
     useEffect(() => {
         async function retrievePublications() {
             const text = await fetch("https://publications-rss.julien-cpsn.com/?username=Julien%20Caposiena").then(r => r.text())
-            console.log(text)
+
             const xmlDoc = new DOMParser().parseFromString(text, "text/xml")
             const local_items = Array.from(xmlDoc.querySelectorAll("item")).map(item => ({
                 title: item.querySelector("title").textContent,
                 author: item.querySelector("author").childNodes[0].data,
-                description: item.querySelector("description").childNodes[0].data,
+                content: item.querySelector("encoded").childNodes[0].data,
+                description: item.querySelector("description")?.childNodes[0].data,
+                pubDate: item.querySelector("pubDate").childNodes[0].data,
+                source: {
+                    name: item.querySelector("source").childNodes[0].data,
+                    url: "https://" + item.querySelector("source").attributes['url'].value
+                },
                 link: item.querySelector("link").childNodes[0].data,
             }))
 
@@ -32,8 +38,8 @@ function getPublications() {
             {items.map((item, index) => (
                 <div key={item.title}>
                     <h3><a href={item.link} target="_blank">{item.title}</a></h3>
-                    <div style={{ opacity: "50%" }}>{ item.author }</div>
-                    <div>{ item.description }</div>
+                    <div style={{ opacity: "50%" }}>{ item.author } - { item.description } - { item.pubDate } - <a href={item.source.url} target="_blank">{ item.source.name }</a></div>
+                    <div>{ item.content }</div>
                     { index !== items.length-1 &&
                         <hr/>
                     }
@@ -49,7 +55,7 @@ export default function Publications() {
             title="Publications"
             description="Scientific publications">
             <main className="main-page">
-                <div style={{ width: "75%", marginLeft: "auto", marginRight: "auto", marginTop: "50px", marginBottom: "50px" }}>
+                <div style={{ width: "80%", marginLeft: "auto", marginRight: "auto", marginTop: "50px", marginBottom: "50px" }}>
                     {getPublications()}
                 </div>
             </main>
